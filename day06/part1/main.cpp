d222d38aae993bdc634583c8b7a68518a72bfb9c
Deniz Yucel <deniz.yucel@partner.bmw.de>
2022-12-06 10:45:41 +0300

    day6 is added

---
+++ day06/part1/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+std::string load(const char* filepath)
+{
+    std::string input;
+
+    auto file = std::ifstream{filepath};
+    auto line = std::string{};
+
+    if (getline(file,line))
+    {
+        input = line;
+    }
+
+    return input;
+}
+
+bool is_all_unique(std::string& str)
+{
+    bool result = true;
+    std::unordered_map<char,int> map;
+
+    for (size_t i = 0; i < str.length(); i++)
+    {
+        if (map.find(str[i]) == map.end())
+        {
+            map[str[i]] = 1;
+        }
+        else
+        {
+            result = false;
+            break;
+        }
+    }
+
+    return result;
+}
+
+int calculate(std::string& input)
+{
+    int pos = -1;
+    std::string marker = input.substr(0,3);
+
+    for (size_t i = 3; i < input.length(); i++)
+    {
+        marker += input[i];
+        if (is_all_unique(marker))
+        {
+            pos = i+1;
+            std::cout << "marker: " << marker << std::endl;
+            break;
+        }
+
+        marker = marker.substr(1,3);
+    }
+    return pos;
+}
+
+int main(int argc, char** argv)
+{
+    if (argc < 2)
+    {
+        std::cout << "invalid argument count" << std::endl;
+        exit(1);
+    }
+    
+    auto input = load(argv[1]);
+    int result = calculate(input);
+    std::cout << result << std::endl;
+}
